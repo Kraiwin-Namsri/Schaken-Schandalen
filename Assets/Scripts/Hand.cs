ee2680b2eede663b8aa27d4ec6f57a1a5ba5a720
Maximillian101 <mb@bischot.nl>
2022-12-27 17:27:52 +0100

    Anoying hand position

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+
+    public static Dictionary<GameObject, Vector2Int> handCoordinates = new Dictionary<GameObject, Vector2Int>();
+
+    public Hand()
+    {
+
+    }
+
+    private void Start()
+    {
+        //GameManager.setParentPointerfinger(this);
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+        GameManager.OnPointerfingerCollisionEnter(this);
+    }
+    public void OnTriggerExit(Collider collision)
+    {
+        //GameManager.OnPointerfingerCollisionExit(this);
+    }
+}
